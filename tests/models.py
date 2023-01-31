bee28d512d328f41047412ad64ebbdf6b377483f
Lyonstead <Tommylyons02@gmail.com>
2023-01-31 20:38:26 +0000

    Setup basic unit testing

---
+++ tests/models.py
@@ -0,0 +1,9 @@
+from flask_login import UserMixin
+
+from extensions import db
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password_hash = db.Column(db.String(150))
