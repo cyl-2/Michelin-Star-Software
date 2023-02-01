1c4efe43c743260cb524f2f1ce43b4f0c069f41b
Aodh <120323406@umail.ucc.ie>
2023-02-01 12:31:27 +0000

    polished off chefs software, now fully functional

--- app.py
+++ app.py
@@ -605,6 +605,54 @@ def confirm_code(email,random_code,table):
             return redirect(url_for("change_password", table=table))
     return render_template("password_management/confirm_code.html", form=form, title= "Confirm code")
 
+@app.route('/kitchen', methods=['GET','POST'])
+def kitchen():
+    cur = mysql.connection.cursor()
+    cur.execute('''SELECT d.name, o.time, d.cook_time, o.tableNo, o.notes, o.status, o.dish_id
+                    FROM orders as o 
+                    JOIN dish as d 
+                    ON o.dish_id=d.dish_id
+                    ORDER BY o.notes='priority' DESC,
+                            o.time,
+                            o.time*d.cook_time DESC,
+                            o.tableNo,  
+                            d.cook_time DESC,
+                            d.category='starter',
+                            d.category='main',
+                            d.category='side',
+                            d.category='dessert';''')
+    orderlist=cur.fetchall()
+
+    return render_template('kitchen.html',orderlist=orderlist)
+
+@app.route('/<int:dish_id>,<int:time>/kitchenUpdate', methods=['GET','POST'])
+
+def kitchenUpdate(dish_id, time):
+    cur = mysql.connection.cursor()
+    cur.execute('''UPDATE orders
+                                SET status="ongoing" 
+                                WHERE time=%s and status="unmade" 
+                                and dish_id=%s 
+                                LIMIT 1;''',(time, dish_id))
+    mysql.connection.commit()
+
+    return redirect(url_for('kitchen'))
+
+@app.route('/<int:dish_id>,<int:time>/kitchenDelete', methods=['GET','POST'])
+def kitchenSentOut(dish_id, time):
+
+    cur = mysql.connection.cursor()
+
+    cur.execute('''UPDATE orders
+                                SET status="sent out"
+                                WHERE time=%s and status="ongoing" 
+                                and dish_id =%s
+                                LIMIT 1;''',(time, dish_id))
+    mysql.connection.commit()
+
+    return redirect(url_for('kitchen'))
+
+
 ##############################################################################################################################################
 ##############################################################################################################################################
 ##############################################################################################################################################
