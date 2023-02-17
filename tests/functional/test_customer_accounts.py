671aeabd4e570e03b8f9fe2548ffcb396172cb03
cyl1 <120377796@umail.ucc.ie>
2023-02-17 00:58:21 +0000

    wrote some request tests, route tests and form validation tests

---
+++ tests/functional/test_customer_accounts.py
@@ -0,0 +1,60 @@
+from tests.extensions import *
+from flask_testing import TestCase
+from app import *
+from tests.instance import BaseTestCase
+from flask import request, url_for
+
+client = app.test_client()
+
+# User details for testing
+Person1 = dict(email='ccc', password='blah',confirm='blah',first_name='mary', last_name='smith')
+Person2 = dict(email='cccccc@gmail.com', password='',confirm='',first_name='mary',last_name='smith')
+Person3 = dict(email='', password='')
+
+''' 
+    I am unable to test: 
+    #def test_registration_form_redirects_if_valid(self):
+    #def test_login_form_redirects_if_valid(self):
+''' 
+class TestCustomerFeatures(MockDB, BaseTestCase):
+
+    def test_registration_page_loads(self):
+        response = client.get('/registration')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Registration", response.data)
+        self.assert_template_used('customer/registration.html')
+
+    def test_registration_page_has_form(self):
+        with client.get('/registration'):
+            form = RegistrationForm()
+
+            self.assertIsInstance(form, RegistrationForm)
+        
+    def test_registration_form_returns_form_errors_if_invalid(self):
+        response1 = client.post('/registration', data=Person1, follow_redirects=True)
+        response2 = client.post('/registration', data=Person2, follow_redirects=True)
+
+        self.assertIn(b'Please enter a valid email address!', response1.data)
+        self.assertIn(b'This field is required', response2.data)
+    
+    def test_login_page_loads(self):
+        response = client.get('/customer_login')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Customer Login", response.data)
+        self.assert_template_used('customer/customer_login.html')
+
+    def test_login_page_has_form(self):
+        with client.get('/customer_login'):
+            form = LoginForm()
+
+            self.assertIsInstance(form, LoginForm)
+    
+    def test_login_form_returns_form_errors_if_invalid(self):
+        response = client.post('/customer_login', data=Person3, follow_redirects=True)
+        self.assertIn(b'This field is required', response.data)
+    
+    def test_profile_page_doesnt_load_if_not_logged_in(self):
+        response = client.get('/customer_profile')
+        self.assertEqual(response.status_code, 500)
---
+++ tests/functional/test_manager_features.py
@@ -0,0 +1,33 @@
+from tests.extensions import *
+from flask_testing import TestCase
+from app import *
+from tests.instance import BaseTestCase
+from flask import request, url_for
+import re
+
+client = app.test_client()
+
+# Sample form info for testing
+form1 = dict(first_name='', last_name='', email='', role='', access_level='')
+
+class TestManagerFeatures(MockDB, BaseTestCase):
+    def test_add_new_staff_page_loads(self):
+        response = client.get('/add_new_employee')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"New Employee Registration", response.data)
+        self.assert_template_used('manager/add_new_staff.html')
+
+    def test_add_new_staff_page_has_form(self):
+        with client.get('/add_new_employee'):
+            form = EmployeeForm()
+
+            self.assertIsInstance(form, EmployeeForm)
+
+    def test_add_new_staff_form_returns_form_errors_if_invalid(self):
+        response = client.post('/add_new_employee', data=form1)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'This field is required', response.data)
+        password = get_random_password()
+        self.assertNotEqual("132123212", password)
